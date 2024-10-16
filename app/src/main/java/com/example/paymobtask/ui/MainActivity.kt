d837209a3bc0519774a329eee94f6bfe668fbc09
mohamed.mansour <mohamed.mansour@expertapps.com.sa>
2024-10-16 18:39:47 +0300

    first push test

---
+++ app/src/main/java/com/example/paymobtask/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.paymobtask.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import com.example.paymobtask.R
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
